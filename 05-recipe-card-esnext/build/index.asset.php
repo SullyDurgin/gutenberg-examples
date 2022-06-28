6570e3bbc9db321ea6941c550e4fad4428e9f36a
SullyDurgin <katsdurgin@gmail.com>
2022-06-27 23:30:35 -0400

    task 4

--- 05-recipe-card-esnext/build/index.asset.php
+++ 05-recipe-card-esnext/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '371e878095670166f208');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '92f992440d95ef9b6e3b');
